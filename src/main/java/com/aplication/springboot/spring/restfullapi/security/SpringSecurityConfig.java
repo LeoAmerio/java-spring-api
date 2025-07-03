122abec6af6dd951bd0317c71b2d8a96f27882db
LeoAmerio14 <leoamerio14@gmail.com>
2025-07-03 11:19:44 -0300

    login validation & endpoint

--- src/main/java/com/aplication/springboot/spring/restfullapi/security/SpringSecurityConfig.java
+++ src/main/java/com/aplication/springboot/spring/restfullapi/security/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.aplication.springboot.spring.restfullapi.security;
 
+import com.aplication.springboot.spring.restfullapi.security.filter.JWTAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +19,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecurityConfig {
 
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -26,6 +38,7 @@ public class SpringSecurityConfig {
                 auth.requestMatchers(HttpMethod.GET,"/api/users/**").permitAll()
                 .requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
                 .anyRequest().authenticated())
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .csrf(config -> config.disable())
                 .cors(cors -> cors.disable())
 //                .csrf(AbstractHttpConfigurer::disable)
--- src/main/java/com/aplication/springboot/spring/restfullapi/security/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/aplication/springboot/spring/restfullapi/security/filter/JWTAuthenticationFilter.java
@@ -2,19 +2,30 @@ package com.aplication.springboot.spring.restfullapi.security.filter;
 
 import com.aplication.springboot.spring.restfullapi.entities.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.aplication.springboot.spring.restfullapi.security.TokenJwtConfig.*;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
@@ -38,5 +49,53 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
         return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) authResult.getPrincipal();
+        String username = user.getUsername();
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+
+        Claims claims = Jwts.claims()
+                .add("authorities", roles)
+                .add("username", username)
+                .build();
+//        claims.put();
+
+        String token = Jwts.builder()
+                .subject(username)
+                .claims(claims)
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .issuedAt(new Date())
+                .signWith(SECRET_KEY)
+                .compact();
+
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + token);
+
+        Map<String, String> body = new HashMap<>();
+        body.put("token", token);
+        body.put("username", username);
+        body.put("message", String.format("%s se a logueado correctamente", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(200);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        Map<String, String> body = new HashMap<>();
+        body.put("message", "Error en la auth, user o password incorrectos!");
+        body.put("error", "Failed message!");
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setContentType(CONTENT_TYPE);
+        response.setStatus(401);
+        response.addHeader(HEADER_AUTHORIZATION, PREFIX_TOKEN + "error");
+        response.addHeader("WWW-Authenticate", "Bearer error");
+        response.addHeader("Access-Control-Expose-Headers", HEADER_AUTHORIZATION);
+        response.addHeader("Access-Control-Allow-Headers", HEADER_AUTHORIZATION);
+        response.addHeader("Access-Control-Allow-Origin", "*");
     }
 }
